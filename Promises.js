f6102f17e4c5790bd1521750d30976260f453f2b
Suraj Gaur <surajgaur242@gmail.com>
2023-11-06 10:20:36 +0530

    modified some errors

--- Promises.js
+++ Promises.js
@@ -8,16 +8,16 @@ function PromiseMe(time,data) {
 }
 
 function success(a) {
-   // document.getElementById("para").textContent(a)
+    document.getElementById("para").innerHTML = a;
     console.log(a);
 }
 function failure(b) {
-   // document.getElementById("para").textContent(b)
+    document.getElementById("para").innerHTML = a;
     console.log(b);
 }
 
 function executePromise() {
-    let result = PromiseMe(2000, "Hello Suraj");
+    let result = PromiseMe(2000, "Welcome My Page");
     result.then(success).catch(failure);
 }
 executePromise();
